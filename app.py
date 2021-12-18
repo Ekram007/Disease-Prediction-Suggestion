cf27f69af8d9beaedd9995de86173048c46fa8b8
Ekramul Islam <ei.ekramul.islam@gmail.com>
2021-12-19 01:50:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,64 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["GET","POST"])
 
+def home():
+    return render_template("home.html")
+
+@app.route("/dengue", methods = ["GET","POST"])
+
+def dengue():
+    
+    predictedDisease = 0
+    if request.method == "POST":
+        features = []
+        for i in range(1,8):
+            s = "feature"+ str(i)
+            features.append(float(request.form[s]))
+        
+        
+
+        predictedDisease = model.dengue_prediction(features)
+
+    return render_template("dengue.html", pd = predictedDisease)
+
+
+@app.route("/thyroid", methods = ["GET","POST"])
+
+def thyroid():
+
+    predictedDisease = 0
+    if request.method == "POST":
+        features = []
+
+        for i in range(1,28):
+            s = "feature"+ str(i)
+            features.append(float(request.form[s]))
+
+        predictedDisease = model.thyroid_prediction(features)
+
+    return render_template("thyroid.html", pd = predictedDisease)
+
+
+@app.route("/diabetes", methods = ["GET","POST"])
+
+def diabetes():
+
+    predictedDisease = 0
+    print('ASDASDASDADASDASDSADSA')
+    if request.method == "POST":
+        features = []
+
+        for i in range(1,9):
+            s = "feature"+ str(i)
+            features.append(float(request.form[s]))
+
+        predictedDisease = model.diabetes_prediction(features)
+
+    return render_template("diabetes.html", pd = predictedDisease)
+
+
+@app.route("/all", methods = ["GET","POST"])
+
 def disease():
     predictedDisease = 0
     if request.method == "POST":
@@ -18,7 +76,6 @@ def disease():
         predictedDisease = model.disease_prediction(features)
 
     return render_template("index.html", pd = predictedDisease)
-
 
 
 
