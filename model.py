4c241fe91aaefbeba129c1ce5c6718c224114408
Ekramul Islam <ei.ekramul.islam@gmail.com>
2022-01-02 22:52:25 +0600

    Update model.py

--- model.py
+++ model.py
@@ -39,31 +39,79 @@ def thyroid_prediction(input_data):
 
 def dengue_doc_med(prev_history):
 
-    dengue_doctor = {"Cirrhosis": "Hepatologist", "Diabetes Mellitus": "Endocrinologist", "Thyroid":"Endocrinologist" ,"Glomeroulonephritis":"Nephrologist", "Carcinoma": "Oncologist", "Pregnancy": "Gynecologist",
-                 "Arrythmia":"Cardiologist", "Bone Pain":"Rheumatologist", "Measles":"Infectious Disease Specialist", "Pulmonary Disease":"Pulmonologist" }
+    dengue_doctor = {"Cirrhosis": "Dr. Haridas Saha ,Hepatologist", 
+                    "Diabetes Mellitus": "Dr. Indrajit Prasad , Endocrinologist", 
+                    "Thyroid":"Endocrinologist, Dr. Indrajit Prasad , " ,
+                    "Glomeroulonephritis":"Prof. Dr. Asia Khanam, Nephrologist", 
+                    "Carcinoma": "Dr. AFM Kamal Uddin , Oncologist", 
+                    "Pregnancy": "Dr. Begum Hosne Ara, Gynecologist",
+                    "Arrythmia":"Dr. AKS Zahid Mahmud Khan , Cardiologist", 
+                    "Bone Pain":"Dr. Abu Shahin , Rheumatologist", 
+                    "Measles":"Prof. Dr. Md. Habibur Rahman, Infectious Disease Specialist", 
+                    "Pulmonary Disease":"Dr. Md. Sajedul Islam, Pulmonologist" }
 
-    dengue_medicine = {"Cirrhosis": "Paracetamol, Lactulose", "Diabetes Mellitus": "Paracetamol, Metformin", "Thyroid":"Paracetamol, Thyroxin " ,"Glomeroulonephritis":"Paracetamol, ACE Inhibitors", "Carcinoma": "Paracetamol, Chemotherapy", "Pregnancy": "Paracetamol",
-                 "Arrythmia":"Paracetamol, Encainide", "Bone Pain":"Paracetamol, Tylenol", "Measles":"Paracetamol, Vitamain-A, Measles virus vaccine", "Pulmonary Disease":"Paracetamol, Salmeterol Inhaler" }
+    dengue_medicine = {"Cirrhosis": "Paracetamol, Lactulose, Drink water.", 
+                        "Diabetes Mellitus": "Paracetamol, Metformin, Drink water.", 
+                        "Thyroid":"Paracetamol, Thyroxin, Drink water." ,
+                        "Glomeroulonephritis":"Paracetamol, ACE Inhibitors, Drink water.", 
+                        "Carcinoma": "Paracetamol, Chemotherapy, Drink water.", 
+                        "Pregnancy": "Paracetamol, Drink water.",
+                        "Arrythmia":"Paracetamol, Encainide, Drink water.", 
+                        "Bone Pain":"Paracetamol, Tylenol, Drink water.", 
+                        "Measles":"Paracetamol, Vitamain-A, Measles virus vaccine, Drink water.", 
+                        "Pulmonary Disease":"Paracetamol, Salmeterol Inhaler, Drink water." }
 
     return dengue_doctor[prev_history] , dengue_medicine[prev_history]
 
 def thyroid_doc_med(prev_history):
 
-    thyroid_doctor = {"Chicken Pox": "Dermatologist", "Common Cold": "General Medicine", "Periodontitis":"Dentist" ,"Dengue":"Critical Care Specialist", "Diabetic Retinopathy": "Opthalmologist", "Hay Fever": "Immunologist",
-                 "Schizophrenia":"Psychiatrist", "PCOS":"Gynecologist", "Erectile dysfunction":"Urologist", "Bell's Palsy":"ENT Specialist" }
+    thyroid_doctor = {"Chicken Pox": "Dr. Tasnim Khan, Dermatologist",
+                        "Common Cold": "Dr. Md. Rezaul Karim, General Medicine", 
+                        "Periodontitis":"Dr. Shahriar Karim Sajid, Dentist" ,
+                        "Dengue":"Dr. Shafat Hossain, Critical Care Specialist", 
+                        "Diabetic Retinopathy": "Dr. Shomir Hossain, Opthalmologist", 
+                        "Hay Fever": "Dr. Tareq Alam, Immunologist",
+                        "Schizophrenia":"Dr. Hasan-Al-Mamun, Psychiatrist", 
+                        "PCOS":"Dr. Begum Hosne Ara, Gynecologist", 
+                        "Erectile dysfunction":"Dr. Md. Jahangir Kabir, Urologist", 
+                        "Bell's Palsy":"Dr. AF Mohiuddin Khan, ENT Specialist" }
 
-    thyroid_medicine = {"Chicken Pox": "Acyclovir", "Common Cold": "Fexofenadine", "Periodontitis":"Doxycycline" ,"Dengue":"Paracetamol", "Diabetic Retinopathy": "Ranibizumab", "Hay Fever": "Claritin",
-                 "Schizophrenia":"Procyclidine HCl", "PCOS":"Metformin", "Erectile dysfunction":"Sildenafil", "Bell's Palsy":"Prednisolone" }
+    thyroid_medicine = {"Chicken Pox": "Acyclovir, regular exercise.", 
+                        "Common Cold": "Fexofenadine, regular exercise.", 
+                        "Periodontitis":"Doxycycline, regular exercise." ,
+                        "Dengue":"Paracetamol, regular exercise.", 
+                        "Diabetic Retinopathy": "Ranibizumab, regular exercise.", 
+                        "Hay Fever": "Claritin, regular exercise.",
+                        "Schizophrenia":"Procyclidine HCl, regular exercise.", 
+                        "PCOS":"Metformin, regular exercise.", 
+                        "Erectile dysfunction":"Sildenafil, regular exercise.", 
+                        "Bell's Palsy":"Prednisolone, regular exercise." }
 
     return thyroid_doctor[prev_history], thyroid_medicine[prev_history]
 
 def diabetes_doc_med(prev_history):
 
-    diabetes_doctor = {"COPD": "Nephrologist", "Peripheral Vascular": "Cardiologist", "Dislipidemia":"Cardiologist" ,"Neuropathy":"Neurologist", "Psoriasis": "Dermatologist", "Acne": "Dermatologist",
-                 "Pulpitis":"Dentist", "Glaucoma":"Opthalmologist", "Thyroid":"Endocrinologist", "Coeliac":"Gastroenterologist" }
+    diabetes_doctor = {"COPD": "Prof. Dr. Asia Khanam, Nephrologist", 
+                        "Peripheral Vascular": "Dr. AKS Zahid Mahmud Khan, Cardiologist", 
+                        "Dislipidemia":"Dr. AKS Zahid Mahmud Khan, Cardiologist" ,
+                        "Neuropathy":"Dr. Bahadur Ali Miah, Neurologist", 
+                        "Psoriasis": "Dr. Tasnim Khan, Dermatologist", 
+                        "Acne": "Dr. Tasnim Khan, Dermatologist",
+                        "Pulpitis":"Dr. Shahriar Karim Sajid, Dentist", 
+                        "Glaucoma":"Dr. Shomir Hossain, Opthalmologist", 
+                        "Thyroid":"Dr. Indrajit Prasad, Endocrinologist", 
+                        "Coeliac":"Dr. Chanchal Kumar Ghosh, Gastroenterologist" }
 
-    diabetes_medicine = {"COPD": "Roflumilast", "Peripheral Vascular": "Anti-clotting agents", "Dislipidemia":"Fibrates and Statins " ,"Neuropathy":"Amitriptyline", "Psoriasis": "Cerave Psoriasis Cream", "Acne": "Dapsone",
-                 "Pulpitis":"NSAID's, Acetominophen", "Glaucoma":"Bimatoprost drop", "Thyroid":"Thyroxin", "Coeliac":"Gluten Free Diet" }
+    diabetes_medicine = {"COPD": "Roflumilast, Diet Control & Exercise.", 
+                        "Peripheral Vascular": "Anti-clotting agents, Diet Control & Exercise.", 
+                        "Dislipidemia":"Fibrates and Statins, Diet Control & Exercise. " ,
+                        "Neuropathy":"Amitriptyline, Diet Control & Exercise.", 
+                        "Psoriasis": "Cerave Psoriasis Cream, Diet Control & Exercise.", 
+                        "Acne": "Dapsone, Diet Control & Exercise.",
+                        "Pulpitis":"NSAID's, Acetominophen, Diet Control & Exercise.", 
+                        "Glaucoma":"Bimatoprost drop, Diet Control & Exercise.", 
+                        "Thyroid":"Thyroxin, Diet Control & Exercise.", 
+                        "Coeliac":"Gluten Free Diet, Diet Control & Exercise." }
 
     return diabetes_doctor[prev_history], diabetes_medicine[prev_history]
 
